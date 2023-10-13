d7844886cbc6eb20ee98c4687d617d717d80251b
hanseu9839 <hanseu9839@gmail.com>
2023-10-14 00:38:03 +0900

    도커 Mysql 설정및 application jpa 환경 설정

--- src/main/java/com/realworld/project/ProjectApplication.java
+++ src/main/java/com/realworld/project/ProjectApplication.java
@@ -2,12 +2,22 @@ package com.realworld.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+
+	@GetMapping("/test")
+	public String test(){
+		return "Spring Test";
+	}
+
 
 }
