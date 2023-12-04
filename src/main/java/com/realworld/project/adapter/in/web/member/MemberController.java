ea909784ef485e59fbd3fbebd230a762d4941a35
hanseu9839 <hanseu9839@gmail.com>
2023-12-04 22:37:50 +0900

    회원 가입, 로그인 컨트롤러

--- src/main/java/com/realworld/project/adapter/in/web/member/MemberController.java
+++ src/main/java/com/realworld/project/adapter/in/web/member/MemberController.java
@@ -1,19 +1,15 @@
 package com.realworld.project.adapter.in.web.member;
 
-import com.realworld.project.application.port.in.GetMemberUseCase;
-import com.realworld.project.application.port.in.PostMemberUseCase;
+import com.realworld.project.application.port.in.Member.GetMemberUseCase;
+import com.realworld.project.application.port.in.Member.PostMemberUseCase;
 import com.realworld.project.application.port.in.dto.MemberDTO;
 import com.realworld.project.application.port.in.dto.TokenDTO;
 import com.realworld.project.common.config.jwt.JwtTokenProvider;
+import com.realworld.project.common.utils.response.CommonApiResponse;
 import com.realworld.project.common.utils.response.CommonUtil;
-import com.realworld.project.domain.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,20 +20,21 @@ public class MemberController {
     private final JwtTokenProvider jwtTokenProvider;
     private final PostMemberUseCase postMemberUseCase;
     private final GetMemberUseCase memberUseCase;
-
     @PostMapping("/member")
-    public ResponseEntity memberRegister(@RequestBody MemberDTO memberDto){
+    public CommonApiResponse memberRegister(@RequestBody MemberDTO memberDto){
 
         if(CommonUtil.isEmpty(memberDto)){
 
         }
         postMemberUseCase.saveMember(memberDto);
-        return ResponseEntity.ok(200);
+        return CommonApiResponse.createSuccessWithNoContent();
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody MemberDTO memberDTO){
-
-        return ResponseEntity.ok(200);
+    public CommonApiResponse login(@RequestBody MemberDTO memberDTO){
+        TokenDTO tokenDto =postMemberUseCase.login(memberDTO);
+        return CommonApiResponse.createSuccess(tokenDto);
     }
+
+
 }
