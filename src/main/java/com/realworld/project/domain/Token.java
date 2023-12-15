b138e4aba02d9c8a85f2241838a3150c8fc4cdbc
hanseu9839 <hanseu9839@gmail.com>
2023-12-15 14:36:13 +0900

    Spring Security CORS 모든 URL에서 허용해주도록 변경

--- src/main/java/com/realworld/project/domain/Token.java
+++ src/main/java/com/realworld/project/domain/Token.java
@@ -1,5 +1,6 @@
 package com.realworld.project.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,17 +8,12 @@ import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude
 public class Token {
     private String userId;
     private String grantType;
     private String accessToken;
     private String refreshToken;
-    @Builder
-    Token(String userId, String grantType, String refreshToken, String accessToken){
-        this.userId = userId;
-        this.grantType = grantType;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 }
