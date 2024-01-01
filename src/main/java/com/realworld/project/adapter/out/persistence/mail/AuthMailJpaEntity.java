385451270dd9ff31f01f16b064437be7ca736dc0
hanseu9839 <hanseu9839@gmail.com>
2024-01-01 09:29:32 +0900

    Import 삭제

--- src/main/java/com/realworld/project/adapter/out/persistence/mail/AuthMailJpaEntity.java
+++ src/main/java/com/realworld/project/adapter/out/persistence/mail/AuthMailJpaEntity.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
