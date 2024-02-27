4957eb114d1d97c65bba5f9f45278ad25be6e8dc
hanseu9839 <hanseu9839@gmail.com>
2024-02-28 06:21:23 +0900

    MailTest

---
+++ src/test/java/com/realworld/project/mail/MailTest.java
@@ -0,0 +1,28 @@
+//package com.realworld.project.mail;
+//
+//import com.realworld.project.application.port.in.dto.AuthMailDTO;
+//import com.realworld.project.application.port.in.mail.GetMailUseCase;
+//import com.realworld.project.service.mail.AuthMailService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//
+//
+//@SpringBootTest(properties = "spring.config.location =" + "classpath:/application.yml")
+//class MailTest {
+//    @Autowired
+//    GetMailUseCase getMailUseCase;
+//
+//    @Autowired
+//    AuthMailService authMailService;
+//    @Test
+//    void sendMail(){
+//        // given
+//        String userEmail = "hanseu9839@gmail.com";
+//        String createKey = authMailService.createKey();
+//        // when
+//
+//        // then
+//    }
+//}
