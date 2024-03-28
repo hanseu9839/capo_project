ac8ce81a73ca5cfa81b8985fcd071174c91ba449
hsk9839 <hanseu9839@gmail.com>
2024-03-29 00:19:12 +0900

    GetCardList Paging처리 완료

--- src/main/java/com/realworld/project/adapter/out/persistence/card/CardRepositoryCustom.java
+++ src/main/java/com/realworld/project/adapter/out/persistence/card/CardRepositoryCustom.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRepositoryCustom  {
-    Slice<CardJpaEntity> findAllCardList(Pageable pageable, String search, String category, long seq);
+    List<CardJpaEntity> findAllCardList(Pageable pageable, String search, String category, long seq);
 }
