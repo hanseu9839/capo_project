31d31727ddcc910d27b604ec6ba899390ed74426
hsk9839 <hanseu9839@gmail.com>
2024-04-03 09:49:29 +0900

    [refactor] 아키텍처 변경으로 인한 미사용파일 제거

--- src/main/java/com/realworld/feature/profile/ProfileCommandService.java
+++
@@ -1,8 +0,0 @@
-package com.realworld.feature.profile;
-
-import com.realworld.feature.member.MemberDTO;
-import com.realworld.feature.member.domain.Member;
-
-public interface ProfileCommandService {
-    Member nicknameUpdate(MemberDTO memberDto, String userId);
-}
