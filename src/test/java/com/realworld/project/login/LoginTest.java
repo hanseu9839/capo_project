70b5d77f334f913e99e5dac007ca000c92c70ab7
hsk9839 <hanseu9839@gmail.com>
2024-04-05 16:29:12 +0900

    TestCode 로그인, 회원가입 테스트 Error Check 구현 완료

--- src/test/java/com/realworld/project/login/LoginTest.java
+++ src/test/java/com/realworld/project/login/LoginTest.java
@@ -1,33 +1,16 @@
 package com.realworld.project.login;
 
-import com.realworld.feature.loginout.LoginOutController;
-import com.realworld.feature.loginout.LoginService;
 import com.realworld.feature.loginout.LoginServiceImpl;
 import com.realworld.feature.member.domain.Member;
-import com.realworld.feature.member.repository.LoadMemberPort;
 import com.realworld.feature.member.service.MemberQueryServiceImpl;
-import com.realworld.feature.token.Token;
-import com.realworld.feature.token.service.TokenCommandService;
 import com.realworld.global.code.ErrorCode;
-import com.realworld.global.code.SuccessCode;
-import com.realworld.global.config.SecurityConfig;
 import com.realworld.global.config.exception.CustomLoginExceptionHandler;
-import com.realworld.global.config.jwt.JwtTokenProvider;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
